87241a3b8b85326060688e7fdbf6e507b0de3ef8
Peter Hollemans <peter@terrenusearth.com>
2025-02-23 19:43:58 -0800

    Updated leading tabs versus spaces

--- src/noaa/coastwatch/io/EarthGridSet.java
+++ src/noaa/coastwatch/io/EarthGridSet.java
@@ -44,107 +44,107 @@ import noaa.coastwatch.util.trans.EarthTransform2D;
  */
 public interface EarthGridSet {
 
-	/** 
-	 * An <code>Axis</code> holds information about one of the axes used to 
-	 * select a 2D grid in the set.
-	 */
-	public class Axis<T> implements Iterable<T> {
-		private String name;
-		private String axisType;
-		private String units;
-		private List<T> values;
-		private Class<T> dataType;
+  /** 
+   * An <code>Axis</code> holds information about one of the axes used to 
+   * select a 2D grid in the set.
+   */
+  public class Axis<T> implements Iterable<T> {
+    private String name;
+    private String axisType;
+    private String units;
+    private List<T> values;
+    private Class<T> dataType;
 
-		/** 
-		 * Creates an axis from its properties and data.
-		 * 
-		 * @param name the axis name: time, altitude, etc.
-		 * @param axisType the axis type: Time, Height, Spectral, Pressure, etc.
-		 * @param units the axis units: seconds since ..., meters, millibars, etc.
-		 * @param values the list of axis coordinate values.
-		 * @param dataType the datatype class for the axis values.
-		 */
-		public Axis (String name, String axisType, String units, List<T> values, Class<T> dataType) {
-	    this.name = name;
-	    this.axisType = axisType;
-	    this.units = units;
-	    this.values = values;
-	    this.dataType = dataType;
-		} // Axis 
+    /** 
+     * Creates an axis from its properties and data.
+     * 
+     * @param name the axis name: time, altitude, etc.
+     * @param axisType the axis type: Time, Height, Spectral, Pressure, etc.
+     * @param units the axis units: seconds since ..., meters, millibars, etc.
+     * @param values the list of axis coordinate values.
+     * @param dataType the datatype class for the axis values.
+     */
+    public Axis (String name, String axisType, String units, List<T> values, Class<T> dataType) {
+      this.name = name;
+      this.axisType = axisType;
+      this.units = units;
+      this.values = values;
+      this.dataType = dataType;
+    } // Axis 
 
     public String getName() { return (name); }
     public String getAxisType() { return (axisType); }
     public String getUnits() { return (units); }
-	  @Override
+    @Override
     public Iterator<T> iterator() { return (values.iterator()); }
     public T getValue (int index) { return (values.get (index)); }
     public int getSize() { return (values.size()); }
     public Class<T> getDataType() { return (dataType); }
     public String toString () { return (name); }
 
-	} // Axis class
+  } // Axis class
 
-	/**
-	 * Gets a list of all grid variable names in this set.
-	 * 
-	 * @return the variable names.
-	 */
-	public List<String> getVariables();
+  /**
+   * Gets a list of all grid variable names in this set.
+   * 
+   * @return the variable names.
+   */
+  public List<String> getVariables();
 
-	/**
-	 * Gets a list of axes used to specify the version of a grid needed
-	 * from the {@link #accessGrid} method.
-	 * 
-	 * @param varName the variable name for the axes.
-	 * 
-	 * @return the list of axes, possibly empty if the variable is just a single
-	 * grid and has no axes.
-	 */
-	public List<Axis> getAxes (String varName);
+  /**
+   * Gets a list of axes used to specify the version of a grid needed
+   * from the {@link #accessGrid} method.
+   * 
+   * @param varName the variable name for the axes.
+   * 
+   * @return the list of axes, possibly empty if the variable is just a single
+   * grid and has no axes.
+   */
+  public List<Axis> getAxes (String varName);
 
-	/**
-	 * Gets a version of a grid specified by a set of axes.
-	 * 
-	 * @param varName the variable name for the grid.
-	 * @param axisIndexList the list of axis indicies, one for each axis,
-	 * that specifies which grid to retrieve.  The list may be empty if the
-	 * variable has no axes.
-	 * 
-	 * @return the grid corresponding to the variable name and axes specified.
-	 */
-	public Grid accessGrid (String varName, List<Integer> axisIndexList);
+  /**
+   * Gets a version of a grid specified by a set of axes.
+   * 
+   * @param varName the variable name for the grid.
+   * @param axisIndexList the list of axis indicies, one for each axis,
+   * that specifies which grid to retrieve.  The list may be empty if the
+   * variable has no axes.
+   * 
+   * @return the grid corresponding to the variable name and axes specified.
+   */
+  public Grid accessGrid (String varName, List<Integer> axisIndexList);
 
-	/**
-	 * Releases the resources used by a grid accessed by this set.
-	 * 
-	 * @param grid the grid to release.
-	 */
-	public void releaseGrid (Grid grid);
+  /**
+   * Releases the resources used by a grid accessed by this set.
+   * 
+   * @param grid the grid to release.
+   */
+  public void releaseGrid (Grid grid);
 
-	/**
-	 * Gets the 2D earth transform for the grids in a variable.
-	 * 
-	 * @param varName the variable name for the earth transform.
-	 * 
-	 * @return the earth transform.
-	 */
-	public EarthTransform2D getTransform (String varName);
+  /**
+   * Gets the 2D earth transform for the grids in a variable.
+   * 
+   * @param varName the variable name for the earth transform.
+   * 
+   * @return the earth transform.
+   */
+  public EarthTransform2D getTransform (String varName);
 
-	/**
-	 * Gets the institute/origin of the data in the dataset.
-	 * 
-	 * @return the data institute/origin.
-	 */
-	public String getOrigin();
+  /**
+   * Gets the institute/origin of the data in the dataset.
+   * 
+   * @return the data institute/origin.
+   */
+  public String getOrigin();
 
-	/**
-	 * Gets the source/platform of the data in the dataset.
-	 * 
-	 * @return the data source/platform.
-	 */
-	public String getSource();
+  /**
+   * Gets the source/platform of the data in the dataset.
+   * 
+   * @return the data source/platform.
+   */
+  public String getSource();
 
-	/** Disposes of the resources used by this grid set. */
-	public void dispose();
+  /** Disposes of the resources used by this grid set. */
+  public void dispose();
 
 } // EarthGridSet interface
--- src/noaa/coastwatch/io/EarthGridSetFactory.java
+++ src/noaa/coastwatch/io/EarthGridSetFactory.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.time.Instant;
 
 import ucar.nc2.dataset.CoordinateSystem;
@@ -58,148 +59,148 @@ public class EarthGridSetFactory {
    * 
    * @throws IOException if there was an error accessing the source.
    */
-	public static EarthGridSet createFromSingleSource (String source) throws IOException {
+  public static EarthGridSet createFromSingleSource (String source) throws IOException {
 
-		// Open the source and get the coordinate systems
-		var reader = EarthDataReaderFactory.create (source);
-		var systems = reader.getCoordinateSystems();
-		LOGGER.fine ("Found " + systems.size() + " coordinate systems");
+    // Open the source and get the coordinate systems
+    var reader = EarthDataReaderFactory.create (source);
+    var systems = reader.getCoordinateSystems();
+    LOGGER.fine ("Found " + systems.size() + " coordinate systems");
 
-		// Create a list of variables and axes
-		var variableList = new ArrayList<String>();
-		var variableAxes = new LinkedHashMap<String, List<Axis>>();
-		var geoAxisTypes = List.of (AxisType.GeoX, AxisType.GeoY, AxisType.Lat, AxisType.Lon);
+    // Create a list of variables and axes
+    var variableList = new ArrayList<String>();
+    var variableAxes = new LinkedHashMap<String, List<Axis>>();
+    var geoAxisTypes = List.of (AxisType.GeoX, AxisType.GeoY, AxisType.Lat, AxisType.Lon);
 
-		// Fill in the variable names if there were no coordinate systems
-		if (systems.size() == 0) {
-			variableList.addAll (reader.getAllGrids());
-			for (var varName : variableList) variableAxes.put (varName, new ArrayList<Axis>());
-		} // if
+    // Fill in the variable names if there were no coordinate systems
+    if (systems.size() == 0) {
+      variableList.addAll (reader.getAllGrids());
+      for (var varName : variableList) variableAxes.put (varName, new ArrayList<Axis>());
+    } // if
 
-		// For each system, add to the mapping of variable name to axes lists
-		else {
-			for (var system : systems) {
-				var systemVars = reader.getVariablesForSystem (system);
-				LOGGER.fine ("Processing coordinate system for variables " + Arrays.toString (systemVars.toArray()));
-				variableList.addAll (systemVars);
-				var coordAxes = system.getCoordinateAxes();
-				var axisList = new ArrayList<Axis>();
-				for (var coordAxis : coordAxes) {
-					if (coordAxis.getRank() == 1) {
-						var coordAxis1D = (CoordinateAxis1D) coordAxis;
-					  if (!geoAxisTypes.contains (coordAxis1D.getAxisType())) {
-					    Axis axis;
+    // For each system, add to the mapping of variable name to axes lists
+    else {
+      for (var system : systems) {
+        var systemVars = reader.getVariablesForSystem (system);
+        LOGGER.fine ("Processing coordinate system for variables " + Arrays.toString (systemVars.toArray()));
+        variableList.addAll (systemVars);
+        var coordAxes = system.getCoordinateAxes();
+        var axisList = new ArrayList<Axis>();
+        for (var coordAxis : coordAxes) {
+          if (coordAxis.getRank() == 1) {
+            var coordAxis1D = (CoordinateAxis1D) coordAxis;
+            if (!geoAxisTypes.contains (coordAxis1D.getAxisType())) {
+              Axis axis;
 
-					    // Create a time axis
-					    if (coordAxis1D.getAxisType().equals (AxisType.Time)) {
-					    	var dateList = new ArrayList<Instant>();
-		            var helper = new CoordinateAxisTimeHelper (Calendar.getDefault(), coordAxis1D.getUnitsString());
-		            double[] offsets = coordAxis1D.getCoordValues();
-		            for (var offset : offsets) {
-  		            var date = helper.makeCalendarDateFromOffset (offset);
-	  	            dateList.add (Instant.ofEpochMilli (date.getMillis()));
-	  	          } // for
-	  	          axis = new Axis<Instant> (
-			    				coordAxis1D.getShortName(),
-			    				coordAxis1D.getAxisType().toString(),
-				    			"milliseconds since 1970-01-01T00:00:00Z",
-									dateList,
-				    			Instant.class
-	  	          );
-					    } // if
+              // Create a time axis
+              if (coordAxis1D.getAxisType().equals (AxisType.Time)) {
+                var dateList = new ArrayList<Instant>();
+                var helper = new CoordinateAxisTimeHelper (Calendar.getDefault(), coordAxis1D.getUnitsString());
+                double[] offsets = coordAxis1D.getCoordValues();
+                for (var offset : offsets) {
+                  var date = helper.makeCalendarDateFromOffset (offset);
+                  dateList.add (Instant.ofEpochMilli (date.getMillis()));
+                } // for
+                axis = new Axis<Instant> (
+                  coordAxis1D.getShortName(),
+                  coordAxis1D.getAxisType().toString(),
+                  "milliseconds since 1970-01-01T00:00:00Z",
+                  dateList,
+                  Instant.class
+                );
+              } // if
 
-					    // Create a regular axis
-					    else {
-	    				  axis = new Axis<Double> (
-			    				coordAxis1D.getShortName(),
-			    				coordAxis1D.getAxisType().toString(),
-				    			coordAxis1D.getUnitsString(),
-									Arrays.stream (coordAxis1D.getCoordValues()).boxed().toList(),
-				    			Double.class
-				    		);
-				    	} // else
+              // Create a regular axis
+              else {
+                axis = new Axis<Double> (
+                  coordAxis1D.getShortName(),
+                  coordAxis1D.getAxisType().toString(),
+                  coordAxis1D.getUnitsString(),
+                  Arrays.stream (coordAxis1D.getCoordValues()).boxed().collect (Collectors.toList()),
+                  Double.class
+                );
+              } // else
 
-	    				axisList.add (axis);
-	    				LOGGER.fine ("Found axis " + axis + " of size " + axis.getSize());
+              axisList.add (axis);
+              LOGGER.fine ("Found axis " + axis + " of size " + axis.getSize());
 
-	    			} // if
-	    		} // if
-				} // for
-				if (!axisList.isEmpty()) {
-					for (var varName : systemVars) {
-						variableAxes.put (varName, axisList);
-					} // for
-				} // if
-			} // for
-		} // else
+            } // if
+          } // if
+        } // for
+        if (!axisList.isEmpty()) {
+          for (var varName : systemVars) {
+            variableAxes.put (varName, axisList);
+          } // for
+        } // if
+      } // for
+    } // else
 
-		// Get the earth transform for the file
-		var trans = reader.getInfo().getTransform();
+    // Get the earth transform for the file
+    var trans = reader.getInfo().getTransform();
 
-		// Create a grid set using this information
-		var gridSet = new EarthGridSet () {
+    // Create a grid set using this information
+    var gridSet = new EarthGridSet () {
 
-			@Override
-			public List<String> getVariables() { return (variableList); }
+      @Override
+      public List<String> getVariables() { return (variableList); }
 
-			@Override
-			public List<Axis> getAxes (String varName) { return (variableAxes.get (varName)); }
+      @Override
+      public List<Axis> getAxes (String varName) { return (variableAxes.get (varName)); }
 
-			@Override
-			public Grid accessGrid (String varName, List<Integer> axisIndexList) {
+      @Override
+      public Grid accessGrid (String varName, List<Integer> axisIndexList) {
 
-				LOGGER.fine ("Retrieving grid for " + varName + " with axes " + 
-					Arrays.toString (axisIndexList.toArray()));
+        LOGGER.fine ("Retrieving grid for " + varName + " with axes " + 
+          Arrays.toString (axisIndexList.toArray()));
 
-				var axes = getAxes (varName);
-				var gridVarName = varName;
+        var axes = getAxes (varName);
+        var gridVarName = varName;
 
-				// For grids with multiple axes, append the indices to the variable
-				if (axes.size() != 0) {
-					gridVarName += "_";
-					for (int i = 0; i < axes.size(); i++) {
-						var axisCode = axes.get (i).getAxisType().substring (0, 1);
-						if (axisCode.equals ("G")) axisCode = "Z";
-						gridVarName += "_" + axisCode + axisIndexList.get (i);
-					} // for
-					gridVarName += "_x_x";
-				} // if
+        // For grids with multiple axes, append the indices to the variable
+        if (axes.size() != 0) {
+          gridVarName += "_";
+          for (int i = 0; i < axes.size(); i++) {
+            var axisCode = axes.get (i).getAxisType().substring (0, 1);
+            if (axisCode.equals ("G")) axisCode = "Z";
+            gridVarName += "_" + axisCode + axisIndexList.get (i);
+          } // for
+          gridVarName += "_x_x";
+        } // if
 
-				// Get the grid and return
-				Grid grid;
-				LOGGER.fine ("Retrieving grid " + gridVarName);
-				try { grid = (Grid) reader.getVariable (gridVarName); }
-				catch (IOException e) { throw new RuntimeException ("Error getting grid", e); }
-				return (grid);
+        // Get the grid and return
+        Grid grid;
+        LOGGER.fine ("Retrieving grid " + gridVarName);
+        try { grid = (Grid) reader.getVariable (gridVarName); }
+        catch (IOException e) { throw new RuntimeException ("Error getting grid", e); }
+        return (grid);
 
-			} // accessGrid
+      } // accessGrid
 
-			@Override
-			public void releaseGrid (Grid grid) { grid.dispose(); }
+      @Override
+      public void releaseGrid (Grid grid) { grid.dispose(); }
 
-			@Override
-			public EarthTransform2D getTransform (String varName) { return ((EarthTransform2D) trans); }
+      @Override
+      public EarthTransform2D getTransform (String varName) { return ((EarthTransform2D) trans); }
 
-			@Override
-			public String getOrigin() { return (reader.getInfo().getOrigin()); }
+      @Override
+      public String getOrigin() { return (reader.getInfo().getOrigin()); }
 
-			@Override
-			public String getSource() { return (reader.getInfo().getSource()); }
+      @Override
+      public String getSource() { return (reader.getInfo().getSource()); }
 
-			@Override
-			public void dispose() {
-
-
-				// What to release here?
+      @Override
+      public void dispose() {
 
 
-			} // dispose
+        // What to release here?
 
-		};
 
-		return (gridSet);
+      } // dispose
 
-	} // create
+    };
+
+    return (gridSet);
+
+  } // create
 
   ////////////////////////////////////////////////////////////
 
@@ -213,7 +214,7 @@ public class EarthGridSetFactory {
    * 
    * @throws IOException if there was an error accessing the source.
    */
-	public static EarthGridSet createFromTimeSeries (List<String> sourceList) throws IOException {
+  public static EarthGridSet createFromTimeSeries (List<String> sourceList) throws IOException {
 
     var gridNames = new TreeSet<String>();
     var infoList = new ArrayList<EarthDataInfo>();
@@ -229,8 +230,8 @@ public class EarthGridSetFactory {
       EarthTransform thisTrans = info.getTransform();
       if (firstTrans == null) firstTrans = thisTrans;
       else {
-      	if (!thisTrans.equals (firstTrans))
-      	  throw (new RuntimeException ("Found earth transform mismatch between " + source + " and " + sourceList.get (0)));
+        if (!thisTrans.equals (firstTrans))
+          throw (new RuntimeException ("Found earth transform mismatch between " + source + " and " + sourceList.get (0)));
       } // else
       gridNames.addAll (reader.getAllGrids());
     } // for
@@ -238,11 +239,11 @@ public class EarthGridSetFactory {
 
     // Eliminate any grid names that are not in all sources
     for (var reader : readerMap.values()) {
-    	var gridsInReader = reader.getAllGrids();
-    	gridNames.retainAll (gridsInReader);
+      var gridsInReader = reader.getAllGrids();
+      gridNames.retainAll (gridsInReader);
     } // for
     if (gridNames.isEmpty())
-    	throw new RuntimeException ("No single variable name found in all input sources");
+      throw new RuntimeException ("No single variable name found in all input sources");
     var variableList = new ArrayList<String> (gridNames);
 
     // Sort the info items by date and create an axis of date values
@@ -250,76 +251,76 @@ public class EarthGridSetFactory {
     for (int i = 0; i < sourceList.size(); i++)
       entryList.add (new SimpleEntry<> (sourceList.get (i), infoList.get (i)));
     entryList.sort (Comparator.comparing (Entry::getValue, Comparator.comparing (EarthDataInfo::getDate)));
-   	var dateList = new ArrayList<Instant>();
-   	for (var entry : entryList)
-   		dateList.add (entry.getValue().getDate().toInstant());
+    var dateList = new ArrayList<Instant>();
+    for (var entry : entryList)
+      dateList.add (entry.getValue().getDate().toInstant());
     var timeAxis = new Axis<Instant> (
-			"time",
-			AxisType.Time.toString(),
-			"milliseconds since 1970-01-01T00:00:00Z",
-			dateList,
-			Instant.class
+      "time",
+      AxisType.Time.toString(),
+      "milliseconds since 1970-01-01T00:00:00Z",
+      dateList,
+      Instant.class
     );
-		var axisList = new ArrayList<Axis>();
-		axisList.add (timeAxis);
-		LOGGER.fine ("Found axis " + timeAxis + " of size " + timeAxis.getSize());
+    var axisList = new ArrayList<Axis>();
+    axisList.add (timeAxis);
+    LOGGER.fine ("Found axis " + timeAxis + " of size " + timeAxis.getSize());
 
-		// Create a grid set using this information
-		var gridSet = new EarthGridSet () {
+    // Create a grid set using this information
+    var gridSet = new EarthGridSet () {
 
-			@Override
-			public List<String> getVariables() { return (variableList); }
+      @Override
+      public List<String> getVariables() { return (variableList); }
 
-			@Override
-			public List<Axis> getAxes (String varName) { return (axisList); }
+      @Override
+      public List<Axis> getAxes (String varName) { return (axisList); }
 
-			@Override
-			public Grid accessGrid (String varName, List<Integer> axisIndexList) {
+      @Override
+      public Grid accessGrid (String varName, List<Integer> axisIndexList) {
 
-				LOGGER.fine ("Retrieving grid for " + varName + " with axes " + 
-					Arrays.toString (axisIndexList.toArray()));
+        LOGGER.fine ("Retrieving grid for " + varName + " with axes " + 
+          Arrays.toString (axisIndexList.toArray()));
 
-				// Get the grid and return
-				int timeIndex = axisIndexList.get (0);
-				var reader = readerMap.get (entryList.get (timeIndex).getKey());
-				Grid grid;
-				LOGGER.fine ("Retrieving grid " + varName);
-				try { grid = (Grid) reader.getVariable (varName); }
-				catch (IOException e) { throw new RuntimeException ("Error getting grid", e); }
-				return (grid);
+        // Get the grid and return
+        int timeIndex = axisIndexList.get (0);
+        var reader = readerMap.get (entryList.get (timeIndex).getKey());
+        Grid grid;
+        LOGGER.fine ("Retrieving grid " + varName);
+        try { grid = (Grid) reader.getVariable (varName); }
+        catch (IOException e) { throw new RuntimeException ("Error getting grid", e); }
+        return (grid);
 
-			} // accessGrid
+      } // accessGrid
 
-			@Override
-			public void releaseGrid (Grid grid) { grid.dispose(); }
-
-
-			// TODO: Could we better handle opening / closing readers here?
+      @Override
+      public void releaseGrid (Grid grid) { grid.dispose(); }
 
 
-			@Override
-			public EarthTransform2D getTransform (String varName) { return ((EarthTransform2D) trans); }
-
-			@Override
-			public String getOrigin() { return (infoList.get (0).getOrigin()); }
-
-			@Override
-			public String getSource() { return (infoList.get (0).getSource()); }
-
-			@Override
-			public void dispose() {
+      // TODO: Could we better handle opening / closing readers here?
 
 
-				// What to release here?
+      @Override
+      public EarthTransform2D getTransform (String varName) { return ((EarthTransform2D) trans); }
+
+      @Override
+      public String getOrigin() { return (infoList.get (0).getOrigin()); }
+
+      @Override
+      public String getSource() { return (infoList.get (0).getSource()); }
+
+      @Override
+      public void dispose() {
 
 
-			} // dispose
+        // What to release here?
 
-		};
 
-		return (gridSet);
+      } // dispose
 
-	} // create
+    };
+
+    return (gridSet);
+
+  } // create
 
   ////////////////////////////////////////////////////////////
 
